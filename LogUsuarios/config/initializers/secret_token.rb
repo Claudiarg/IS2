f58e6127ba4d12efe0493c6a7958d7f828e1c058
Claudiarg <claudia.cirg@gmail.com>
2015-10-19 11:54:31 -0500

    Creamos el proyecto

---
+++ LogUsuarios/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LogUsuarios::Application.config.secret_key_base = '236347945341ba4116249dde113d50c76724ae69bc2acad78667a359f247355706de7f3e2eba4e49b4499cde5ca8d7dc72f45cf82706e12ff3dee7dc209554ef'
